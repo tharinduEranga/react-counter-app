1a486fb2f21382038c356904617606631f63dff1
tharinduEranga <tharindueranga3@gmail.com>
2021-01-06 11:55:46 +0530

    added home component

---
+++ src/components/home.jsx
@@ -0,0 +1,13 @@
+import {Component} from "react/cjs/react.production.min";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Java</h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
